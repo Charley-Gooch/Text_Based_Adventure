08c129910aea4ed71d5ffc7f163c3e87de5e83b7
Charley Gooch <chazgooch@gmail.com>
2018-10-02 14:35:44 +0000

    character creator completed

--- bin/ruby/app/Character_Creation.rb
+++ bin/ruby/app/Character_Creation.rb
@@ -1,17 +1,26 @@
-require_relative "Entity"
-require_relative "Item"
 require_relative "Text_Printer"
 
+$confirmation = false
 def ccStart
-  confirmation = false
   text_print("classes.txt")
-  while confirmation == false do
-    selection = gets.chomp.downcase
-    if selection == "brute" || selection == "ranger" || selection == "warrior" || selection == "wizard"
-      text_print("class_#{selection}.txt")
+  processing()
+end
+$selection = nil
+def processing
+  while $confirmation == false do
+    if $selection == nil
+    $selection = gets.chomp.downcase
+    end
+    if $selection == "brute" || $selection == "ranger" || $selection == "warrior" || $selection == "wizard"
+      text_print("class_#{$selection}.txt")
       puts "to confirm your choice, please type 'confirm', else type another class to view"
-      if gets.chomp.downcase == "confirm"
-        return selection
+      conf = gets.chomp.downcase
+      if conf == "confirm"
+        $confirmation = true
+        return $selection
+      else
+        $selection = conf
+        processing()
       end
     else
       puts "this is not a valid class. please enter: brute, ranger, warrior or wizard."
